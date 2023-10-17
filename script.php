5b009c436774a03d1a767ed49c1859c31dea19e8
Joomill <info@joomill.nl>
2023-10-17 09:06:11 +0200

    V2.0.0 - Added IconSelector Field - Load ModalHelper - Load InstallScript to disable install on Joomla < 5.0

---
+++ script.php
@@ -0,0 +1,73 @@
+<?php
+/*
+ *  package: Custom-Quickicons
+ *  copyright: Copyright (c) 2023. Jeroen Moolenschot | Joomill
+ *  license: GNU General Public License version 2 or later
+ *  link: https://www.joomill-extensions.com
+ */
+
+// No direct access.
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Installer\InstallerAdapter;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Log\Log;
+
+class mod_custom_quickiconInstallerScript
+{
+    /**
+     * Minimum Joomla version to check
+     *
+     * @var    string
+     * @since  5.0.0
+     */
+    private $minimumJoomlaVersion = '5.0';
+
+    /**
+     * Minimum PHP version to check
+     *
+     * @var    string
+     * @since  5.0.0
+     */
+    private $minimumPHPVersion = JOOMLA_MINIMUM_PHP;
+
+
+    /**
+     * Function called before extension installation/update/removal procedure commences
+     *
+     * @param string $type The type of change (install, update or discover_install, not uninstall)
+     * @param InstallerAdapter $parent The class calling this method
+     * @return  boolean  True on success
+     * @throws Exception
+     * @since  5.0.0
+     */
+    public function preflight($type, $parent): bool
+    {
+        if ($type !== 'uninstall')
+        {
+            // Check for the minimum PHP version before continuing
+            if (!empty($this->minimumPHPVersion) && version_compare(PHP_VERSION, $this->minimumPHPVersion, '<'))
+            {
+                Log::add(
+                    Text::sprintf('JLIB_INSTALLER_MINIMUM_PHP', $this->minimumPHPVersion),
+                    Log::WARNING,
+                    'jerror'
+                );
+                return false;
+            }
+            // Check for the minimum Joomla version before continuing
+            if (!empty($this->minimumJoomlaVersion) && version_compare(JVERSION, $this->minimumJoomlaVersion, '<'))
+            {
+                Log::add(
+                    Text::sprintf('JLIB_INSTALLER_MINIMUM_JOOMLA', $this->minimumJoomlaVersion),
+                    Log::WARNING,
+                    'jerror'
+                );
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
